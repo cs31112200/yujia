2c951e2441a4b88687021ff345e5d63f9f73daca
chensl <103479641@qq.com>
2018-11-10 13:43:11 +0800

    1111

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,6 +8,9 @@ use think\Db;
 class Index extends Controller
 {
     
+	public function index(){
+		echo "hello,world";
+	}
     
     public function tuisong_test(){
         for($i=1;$i<=20;$i++){
